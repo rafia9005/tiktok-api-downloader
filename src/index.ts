81fb9f3a2a3b1a9f4ab67a7897253c9d2b7d24c7
rafia9005 <rafia9005@gmail.com>
2025-05-03 12:22:02 +0000

    add controller

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { downloaderController } from './controller/downloader-controller.js'
 
 const app = new Hono()
 
@@ -7,9 +8,11 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.route('/api', downloaderController)
+
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3005
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
